4fd857a67be263a8b5df9db536bfc2a9cf069bca
jdurant0529 <james.durant@gmail.com>
2016-06-04 18:22:15 -0400

    movie stuff working, but needs completed.

--- key.js
+++ key.js
@@ -1,9 +1,18 @@
-console.log('this is loaded');
+//console.log('this is loaded');
 
 module.exports = {
-  consumer_key: 'vdoSppAXpifESaLEYL8Z3yIAQ',
-  consumer_secret: 'bezqbTXYqabEKyYbnnYaaATWKTxvcQP4xoctPislXURj9myalT',
-  access_token_key: '94437895-oAULbeG0EYbYh4CNJap9QSZBe8lS7gWL9IHvQnD6H',
-  access_token_secret: 'HdApGsI5pUZszZIstNCxo3oU8pGyAVYufury0rn5Wj8Ri',
+	tweetKey: {
+	  consumer_key: 'vdoSppAXpifESaLEYL8Z3yIAQ',
+	  consumer_secret: 'bezqbTXYqabEKyYbnnYaaATWKTxvcQP4xoctPislXURj9myalT',
+	  access_token_key: '94437895-oAULbeG0EYbYh4CNJap9QSZBe8lS7gWL9IHvQnD6H',
+	  access_token_secret: 'HdApGsI5pUZszZIstNCxo3oU8pGyAVYufury0rn5Wj8Ri',
+	},
+	tweetKey: {
+	  consumer_key: 'vdoSppAXpifESaLEYL8Z3yIAQ',
+	  consumer_secret: 'bezqbTXYqabEKyYbnnYaaATWKTxvcQP4xoctPislXURj9myalT',
+	  access_token_key: '94437895-oAULbeG0EYbYh4CNJap9QSZBe8lS7gWL9IHvQnD6H',
+	  access_token_secret: 'HdApGsI5pUZszZIstNCxo3oU8pGyAVYufury0rn5Wj8Ri',
+	},
 }
+
 
--- liri.js
+++ liri.js
@@ -3,40 +3,68 @@ var LLL = require('./key.js');
 var option = process.argv[2];
 var search = process.argv[3];
 
-console.log(option);
-console.log(search);
+//console.log(option);
+//console.log(search);
 
 
 
 if (option == 'my-tweets') {
-    console.log('tweet tweet');
-    var Twit = require('twitter')
-    var config = require('./key.js');
-
-    var T = new Twit(config)
-       // console.log(T);
-    T.get('statuses/user_timeline', { screen_name: 'JRDurant', count: 2 }, function(err, data, response) {
-
-          for (var i = 0; i < data.length; i++)
-
-          console.log(data[i].text);
-        })
+    tweet();
 
 } else if (option == 'spotify-this-song') {
-    console.log('spotifying')
+
     if (search == null) {
         search = "what's my age again"
     }
-    console.log(search);
+    song(search);
+
 } else if (option == 'movie-this') {
-    console.log('movie stuff');
     if (search == null) {
         search = 'Mr. Nobody'
     }
-    console.log(search);
+    movie(search);
+    
 } else if (option == 'do-what-it-says') {
     console.log('doing what it says');
 } else {
     console.log('You dont follow directions very well.')
 }
 ///console.log(LLL.twitterKeys);
+
+
+
+function movie(search) {
+    var request = require('request');
+    
+    var url = 'http://www.omdbapi.com/?t=' + search + '&y=&plot=short&r=json';
+
+    request(url, function(err, response, body) {
+        //console.log(body);
+        body = JSON.parse(body);
+        console.log(body.Title);
+        console.log(body.Released);
+    })
+}
+
+function song(search) {
+    console.log('spotifying')
+}
+
+function tweet() {
+    var Twit = require('twitter')
+    var config = require('./key.js');
+
+    var T = new Twit(config.tweetKey)
+
+    T.get('statuses/user_timeline', { screen_name: 'JRDurant', count: 20 }, function(err, data, response) {
+
+        for (var i = 0; i < data.length; i++)
+
+            console.log(data[i].created_at + " " + data[i].text);
+    })
+
+}
+
+function sayWhat() {
+
+}
